141497cdc4cff07cfbb1bd34333ea1115bec0130
Sudeep Surisetty <sudeepgupta.surisetty@hmhco.com>
2019-11-14 13:27:13 -0500

    Initial commit

--- sgm-interest-data/src/main/java/com/hmhco/sgminterest/domain/User.java
+++ sgm-interest-data/src/main/java/com/hmhco/sgminterest/domain/User.java
@@ -3,47 +3,69 @@
  */
 package com.hmhco.sgminterest.domain;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 /**
  * @author damodaranv
  *
  */
+@Document(collection = "userQuestionAnswer")
 public class User {
 	
-	private String id;
+	@Id
+	private String userId;
+	private String firstName;
+	private String lastName;
+	private String grade;
+	private List<Question> questionList;
+	
+	public String getUserId() {
+		return userId;
+	}
 
-    private String name;
-    
-    private String email;
-    
-	public User(String id, String name, String email) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.email = email;
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+
+	public List<Question> getQuestionList() {
+		return questionList;
+	}
+
+	public void setQuestionList(List<Question> questionList) {
+		this.questionList = questionList;
 	}
 	
-	public String getId() {
-		return id;
-	}
+	
 
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-    
 }
