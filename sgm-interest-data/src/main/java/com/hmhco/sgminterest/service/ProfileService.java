058bc5b808e2274c1ab6735590c599e384d5dae6
kalpanamurthyn <kalpanamurthyn@gmail.com>
2019-11-14 16:08:38 -0500

    added getProfilesByCategoryAndSubCategory

--- sgm-interest-data/src/main/java/com/hmhco/sgminterest/service/ProfileService.java
+++ sgm-interest-data/src/main/java/com/hmhco/sgminterest/service/ProfileService.java
@@ -1,5 +1,10 @@
 package com.hmhco.sgminterest.service;
 
+import java.util.List;
+
+import com.hmhco.sgminterest.persistence.Profile;
+
 public interface ProfileService {
 
+	List<Profile> getProfilesByCategoryAndSubCategory(String category, String subCategory );
 }
--- sgm-interest-data/src/main/java/com/hmhco/sgminterest/service/ProfileServiceImpl.java
+++ sgm-interest-data/src/main/java/com/hmhco/sgminterest/service/ProfileServiceImpl.java
@@ -1,5 +1,24 @@
 package com.hmhco.sgminterest.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hmhco.sgminterest.persistence.Profile;
+import com.hmhco.sgminterest.persistence.ProfileRepository;
+
+@Service
 public class ProfileServiceImpl implements ProfileService{
 
+	@Autowired
+	private ProfileRepository profileRepository;
+	
+	@Override
+	public List<Profile> getProfilesByCategoryAndSubCategory(String category, String subCategory) {
+		List<Profile> profiles = profileRepository.findByCategoryAndSubCategory(category, subCategory);	
+		return profiles;		
+	}
+
+	
 }
